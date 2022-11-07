3b78a1c390bec97b1e0e447633bf04a14f23abfe
Roman Timashoff <timashoff@gmail.com>
2022-11-08 01:00:45 +0600

    feat: add files-in-folder solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,36 @@
+import fs from 'fs/promises'
+import path from 'path'
+import url from 'url'
+
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const pathToFolder = path.join(__dirname, 'secret-folder');
+
+(async () => {
+  try {
+    const files = await fs.readdir(pathToFolder, { withFileTypes: true })
+    files.forEach(async (file) => {
+      if (file.isFile()) {
+        const name = file.name
+        const type = path.extname(name)
+        const size = (await fs.stat(path.join(pathToFolder, name))).size
+        console.log(`${name} - ${type} - ${formatBytes(size, 3)}`)
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+)()
+
+
+//helpers
+function formatBytes(bytes, decimals = 2) {
+  if (!+bytes) return '0 Bytes'
+  const K = 1024
+  const DM = decimals < 0 ? 0 : decimals
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+  const i = Math.floor(Math.log(bytes) / Math.log(K))
+  return `${parseFloat((bytes / Math.pow(K, i)).toFixed(DM))} ${sizes[i]}`
+}
